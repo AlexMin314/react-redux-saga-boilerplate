81843553da4e47c269199258246b675eea868455
Jun Jie Ng <jjng@palo-it.com>
2018-01-31 17:33:06 +0800

    Add comments to duck

--- src/ducks/count.js
+++ src/ducks/count.js
@@ -3,14 +3,27 @@ import { call, put } from 'redux-saga/effects'
 import { createAction, createReducer } from 'redux-act'
 import { createSagaWatcher } from 'utilities'
 
+/**
+ * Action creators
+ */
 export const increment = createAction('INCREMENT')
 export const requestIncrement = createAction('INCREMENT_REQUESTED')
 
+/**
+ * Initial state
+ */
 const initialState = 0
+
+/**
+ * Reducer
+ */
 export default createReducer({
   [increment]: state => state + 1
 }, initialState)
 
+/**
+ * Sagas
+ */
 export const sagas = createSagaWatcher({
   [requestIncrement]: function* (action) {
     yield call(delay, 1000)
